8e6cbe5d201a010b7961981e00c7f131def3f934
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-10-30 20:04:58 +0530

    Feat: Adding new file type_casting.py in Day 1 folder

---
+++ Day1/practice/type_casting.py
@@ -0,0 +1,25 @@
+# Type casting in python:
+
+var1 = 10
+var2 = 20
+var3 = 30
+
+print("Before Type Casting : ")
+print(" ")
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(" ")
+
+print("After Type Casting : ")
+print(" ")
+
+var1 = str(10)
+var2 = int(20)
+var3 = float(30)
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(" ")
