0583610813f745a9f418ad8aec5a3f6bdfc76d77
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-10-30 19:52:42 +0530

    Feat: Adding new file operaters.py in Day 1 folder

---
+++ Day1/practice/operaters.py
@@ -0,0 +1,13 @@
+# Arithmetic Operators:
+
+num1 = 10
+num2 = 3
+
+print("Addition: ",num1+num2)
+print("Subtraction: ",num1-num2)
+print("Multiplication: ",num1*num2)
+print("Division: ",num1/num2)
+print("Modulus: ",num1%num2)
+print("Exponentiation: ",num1**num2)
+print("Floor division: ",num1//num2)
+
