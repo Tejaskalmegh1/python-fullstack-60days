c75eb9438a7a5ddb56bf160a511b9de795085958
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-10-30 19:17:52 +0530

    Doc: Variable and Data type updated

--- Day1/practice/variables_data_types.py
+++ Day1/practice/variables_data_types.py
@@ -1,1 +1,23 @@
-# variables in python 
+# variables in python
+
+
+# Variables are containers for storing data values
+# Python has no command for declaring a variable
+# Variables do not need to be declared with any particular type,
+# and can even change type after they have been set.
+
+
+name = "Tejas"
+rollno = 7
+marks = 77.77
+eligibility = True
+
+print("Student Name: ",name)
+print("Roll Number: ",rollno)
+print("Marks Obtain: ",marks)
+print("Eligible or not: ",eligibility)
+
+print(type(name))
+print(type(rollno))
+print(type(marks))
+print(type(eligibility))
