01579a5751bdc4e8c2d73cb35b689f372d1daeaa
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-10-30 18:52:23 +0530

    doc: Added explanations about end parameter

--- Day1/practice/basic_print.py
+++ Day1/practice/basic_print.py
@@ -1,5 +1,11 @@
 
 print("This is My First Python Program")
 
-# This is comment 
+# This is comment in python
 # The print statemnt is a built-in function that outputs text or data to the standard output stream
+
+print("1st statement", end=" ")
+print("2nd statement")
+
+# The end parameter is use to print 2 statements on the same line 
+
