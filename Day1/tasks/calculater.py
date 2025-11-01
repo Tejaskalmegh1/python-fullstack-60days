e155124195eceb6531cefb61e674cd6a13afb5f6
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-11-01 12:02:25 +0530

    feat(tasks): add day-3 calculator task

---
+++ Day1/tasks/calculater.py
@@ -0,0 +1,10 @@
+# simple calculator
+
+# Taking input from user in float datatype
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+print("Addition of Two Numbers:", num1 + num2)
+print("Subtraction of Two Numbers:", num1 - num2)
+print("Multiplication of Two Numbers:", num1 * num2)
+print("Division of Two Numbers:", num1 / num2)
