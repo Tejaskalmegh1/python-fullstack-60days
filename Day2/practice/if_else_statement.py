4d491c4cc36515f5dd15754197de3e15ba75741d
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-11-03 11:32:51 +0530

    feat(day2): add if_else_statement.py in practice folder

---
+++ Day2/practice/if_else_statement.py
@@ -0,0 +1,9 @@
+# if else statement
+
+num = -10
+
+
+if num > 0:
+    print("Number is Possitive!") # print this statement if condition is true 
+else:
+    print("Number is Negative or Zero") # print this statement if condition is false
