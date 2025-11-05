ae8af2c3c1afde6ad2ca12eb41470212bec26b43
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-11-05 13:55:22 +0530

    feat(day2): add larg_of_three.py in task

---
+++ Day2/tasks/larg_of_three.py
@@ -0,0 +1,16 @@
+# largest of three numbers
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+if num1 > num2:
+    print(f"{num1} is larger")
+elif num1 > num3:
+    print(f"{num1} is larger")
+elif num2 > num3:
+    print(f"{num2} is larger")
+else:
+    print(f"{num3} is larger")
+
+
