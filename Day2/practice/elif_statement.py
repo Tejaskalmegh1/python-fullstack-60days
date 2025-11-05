32049c607021f9a6bdea2bafe3cb3235530acdd4
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-11-05 10:38:06 +0530

    feat(day2): add elif_statement.py in pratice

---
+++ Day2/practice/elif_statement.py
@@ -0,0 +1,27 @@
+# elif statement
+
+num1 = 55
+
+if (num1 > 100):
+    print("Number is greater than 100")
+elif(num1 > 50):
+    print("Number is greater than 50")
+
+
+# Multiple elif statements 
+
+num2 = 20
+
+if (num2 > 100):
+    print("Number is greater than 100")
+elif(num2 > 80):
+    print("Number is greater than 80")
+elif(num2 > 60):
+    print("Number is greater than 60")
+elif(num2 > 40):
+    print("Number is greater than 40")
+elif(num2 > 20):
+    print("Number is greater than 20")
+else:
+    print("Number is less than 20")
+    
