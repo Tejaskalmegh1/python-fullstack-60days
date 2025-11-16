778380ed82f03f20bec64f23350e81ef4c8ed3d4
Tejas Kalmegh <tejaskalmegh123@gmail.com>
2025-11-16 20:14:23 +0530

    feat(day3): add sum.py in task

---
+++ Day3/tasks/sum.py
@@ -0,0 +1,8 @@
+def addition(num1,num2):
+    return num1 + num2
+
+n1 = int(input("Enter First Number: "))
+n2 = int(input("Enter Second Number: "))
+add = addition(n1,n2)
+
+print("Addition: ", add)
